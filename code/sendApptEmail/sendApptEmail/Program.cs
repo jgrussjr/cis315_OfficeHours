4aa4b35514f3a810c39a6ad01542579be732f2d9
James Gruss, Jr <jgrussjr@gmail.com>
2016-12-16 14:38:52 -0500

    moved mcCracken's email tester program to inside sendApptEmail; moved his spreadsheet to main test cases folder

--- code/sendApptEmail/sendApptEmail/Program.cs
+++ code/sendApptEmail/sendApptEmail/Program.cs
@@ -1,7 +1,4 @@
-﻿// This is a demo class for the classes
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,40 +10,229 @@ namespace sendApptEmail
     {
         static void Main(string[] args)
         {
-            String requestorEmail = "gruss001@knights.gannon.edu";
-            String requestorDisplayName = "James Gruss";
 
-            String requesteeEmail = "mccracke007@knights.gannon.edu";
-            String requesteeDisplayName = "Andrew McCracken";
-
-            //String requesteeEmail = "jgrussjr@gmail.com";
-            //String requesteeDisplayName = "Prof JG";
-
-            //String requesteeEmail = "carlin010@knights.gannon.edu";
-            //String requesteeDisplayName = "Nate Carlin";
-
-
-            //String requestorEmail = "jgrussjr@gmail.com";
-            //String requestorDisplayName = "Google Gruss";
-
-            //String requesteeEmail = "gruss001@knights.gannon.edu";
-            //String requesteeDisplayName = "James Gruss";
-
-
+            String requestorEmail = "mccracke007@knights.gannon.edu";
+            String requestorDisplayName = "STUDENT";
+            String requesteeEmail = "an_sc_mc@yahoo.com";
+            String requesteeDisplayName = "PROFESSOR";
             DateTime apptStart = DateTime.UtcNow.AddMinutes(+30);
             String location = "The Office - test";
-            String messageBodyProf = "Hello, I would like to meet with you to discuss the upcoming data structures assignment.";
+            String messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
             String messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
 
+            string testCaseNum = "0";
+
+            while (testCaseNum != "x")
+            {
+                Console.WriteLine("Enter test case #, or x to exit program: ");
+                testCaseNum = Console.ReadLine();
+
+                switch (testCaseNum)
+                {
+                    case "1":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 1";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "2":
+                        requestorEmail = null;
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 1";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "3":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = null;
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 2";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "4":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = null;
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 3";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "5":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = null;
+                        location = "Test Case 5";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "6":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 6";
+                        messageBodyProf = null;
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "7":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 7";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = null;
+                        break;
+                    case "8":
+                        requestorEmail = null;
+                        requestorDisplayName = null;
+                        requesteeEmail = null;
+                        requesteeDisplayName = null;
+                        location = null;
+                        messageBodyProf = null;
+                        messageBodyStud = null;
+                        break;
+                    case "9":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = null;
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "10":
+                        requestorEmail = "";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 10";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "11":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 11";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "12":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 12";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "13":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "";
+                        location = "Test Case 13";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "14":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 14";
+                        messageBodyProf = "";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "15":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 15";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "";
+                        break;
+                    case "16":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = null;
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "Test Case 16";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "17":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = null;
+                        location = "Test Case 17";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "18":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = null;
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = null;
+                        location = "Test Case 18";
+                        messageBodyProf = "Hello, I would like to meet with you to discuss the assignment.";
+                        messageBodyStud = "You have requested a meeting with " + requesteeDisplayName + " for " + apptStart.ToString();
+                        break;
+                    case "19":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = "";
+                        messageBodyProf = "";
+                        messageBodyStud = "";
+                        break;
+                    case "20":
+                        requestorEmail = "mccracke007@knights.gannon.edu";
+                        requestorDisplayName = "STUDENT";
+                        requesteeEmail = "an_sc_mc@yahoo.com";
+                        requesteeDisplayName = "PROFESSOR";
+                        location = null;
+                        messageBodyProf = null;
+                        messageBodyStud = null;
+                        break;
+                    default:
+                        testCaseNum = "x";
+                        break;
+                }
+
+                EmailSender es = new EmailSender();
 
 
-            EmailSender es = new EmailSender();
+                Console.WriteLine("Execute emailSender? y/n ");
+                string send = Console.ReadLine();
 
-            //es.sendEmailInvite(requestorEmail, requestorDisplayName, requesteeEmail, requesteeDisplayName,
-            //apptStart, location, messageBodyProf, messageBodyStud);
+                if (send == "y" && testCaseNum != "x")
+                {
+                    es.sendEmailInvite(requestorEmail, requestorDisplayName, requesteeEmail, requesteeDisplayName,
+                    apptStart, location, messageBodyProf, messageBodyStud);
 
-            es.sendEmailInvite(requestorEmail, requestorDisplayName, requesteeEmail, requesteeDisplayName,
-            apptStart);
+                    //es.sendEmailInvite(requestorEmail, requestorDisplayName, requesteeEmail, requesteeDisplayName,
+                    //apptStart);
+                }
+                else
+                {
+                    testCaseNum = "x";
+                }
+            }
 
 
         }
